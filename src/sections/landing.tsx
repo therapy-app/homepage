a7dbab7da6536e1226f07f997a99c7c61926e945
MHuesler <hueslermaxim@gmail.com>
2022-01-26 22:06:03 +0100

    added dashboard image

--- src/sections/landing.tsx
+++ src/sections/landing.tsx
@@ -8,52 +8,56 @@ import {
   Link,
   Text,
   VStack,
+  Image
 } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Nav from "../../components/Nav";
 
 const LandingSection: NextPage = () => {
   return (
-    <Box h={"100vh"}>
+    <Box h={"100vh"} >
       <Nav></Nav>
-      <Container maxW={"container.xl"} my={10}>
-        <Flex
-          alignItems={"center"}
-          height={650}
-          justifyContent={"space-between"}
-        >
-          <VStack align={"flex-start"} spacing={5}>
-            <Heading as={"h1"} fontSize="7xl">
-              Let your practice
-              <Text
-                bgGradient="linear(to-l, #36D1DC, #5B86E5)"
-                bgClip="text"
-                fontSize="7xl"
-                fontWeight="extrabold"
-              >
-                {" "}
-                feel relief.
-              </Text>
-            </Heading>
-            <Text fontSize="2xl">Improve your workflow, reduce chaos.</Text>
-            <Link href={"https://my.therapyapp.ch/auth/signup"}>
-              <Button size={"lg"}>
-                <Text>
-                  Join the <i>free</i> alpha
+      <Container maxW={'2000'} pos={'relative'} overflow={'hidden'}>
+        <Container maxW={"container.xl"} my={10}>
+          <Flex
+            alignItems={"center"}
+            height={650}
+            justifyContent={"space-between"}
+          >
+            <VStack align={"flex-start"} spacing={5}>
+              <Heading as={"h1"} fontSize="7xl">
+                Let your practice
+                <Text
+                  bgGradient="linear(to-l, #36D1DC, #5B86E5)"
+                  bgClip="text"
+                  fontSize="7xl"
+                  fontWeight="extrabold"
+                >
+                  {" "}
+                  feel relief.
                 </Text>
-              </Button>
-            </Link>
-          </VStack>
-        </Flex>
-        <Center>
-          <Text fontSize={"1xl"}>
-            <Link href={"https://my.therapyapp.ch/auth/signup"}>
-              Join the alpha
-            </Link>{" "}
-            as an earlybird and enjoy{" "}
-            <Link href={"pricing"}>discounted offers</Link>. 🐦
-          </Text>
-        </Center>
+              </Heading>
+              <Text fontSize="2xl">Improve your workflow, reduce chaos.</Text>
+              <Link href={"https://my.therapyapp.ch/auth/signup"}>
+                <Button size={"lg"}>
+                  <Text>
+                    Join the <i>free</i> alpha
+                  </Text>
+                </Button>
+              </Link>
+            </VStack>
+            <Image right={-75} boxShadow='xl' rounded='md' width={900} pos={'absolute'} zIndex={'-1'} src="dashboard.png" alt="Image showing dashboard."></Image>
+          </Flex>
+          <Center>
+            <Text fontSize={"1xl"}>
+              <Link href={"https://my.therapyapp.ch/auth/signup"}>
+                Join the alpha
+              </Link>{" "}
+              as an earlybird and enjoy{" "}
+              <Link href={"pricing"}>discounted offers</Link>. 🐦
+            </Text>
+          </Center>
+        </Container>
       </Container>
     </Box>
   );
